25066c2e6697aea8240e0e1f90a0faaa582404b8
Xpolo29 <Paul.Guyennet@gmail.com>
2021-11-06 23:37:58 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace bahbah
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BahBah_ico = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // BahBah_ico
+            // 
+            this.BahBah_ico.Icon = ((System.Drawing.Icon)(resources.GetObject("BahBah_ico.Icon")));
+            this.BahBah_ico.Text = "BahBah";
+            this.BahBah_ico.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.BahBah_ico_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(236, 119);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "BahBah";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon BahBah_ico;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+
+namespace bahbah
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+			//hide in sys tray
+            InitializeComponent();
+            reduce();
+
+
+            //create right click menu
+            this.components = new System.ComponentModel.Container();
+            this.ContextMenu = new System.Windows.Forms.ContextMenu();
+			
+			//create menu item "exit"
+            MenuItem menuItem1 = new System.Windows.Forms.MenuItem();
+            menuItem1.Index = 0;
+            menuItem1.Text = "E&xit";
+            menuItem1.Click += new System.EventHandler(this.menuItem1_Click);
+            this.ClientSize = new System.Drawing.Size(292, 266);
+            this.Text = "Notify Icon Example";
+            ContextMenu.MenuItems.Add(menuItem1);
+            BahBah_ico.ContextMenu = this.ContextMenu;
+
+            //When startup is finished, play sound
+            BahBah_ico_MouseDoubleClick(this, new MouseEventArgs(MouseButtons.Left,2,0,0,100));
+        }
+
+		//handle exit menu click
+        private void menuItem1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BahBah_ico_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+			//play sound on double click
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(Properties.Resources.bahbah);
+            player.Play();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            reduce();
+        }
+
+        private void reduce()
+        {
+            Hide();
+            BahBah_ico.Visible = true;
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+            this.FormBorderStyle = FormBorderStyle.SizableToolWindow;
+        }
+    }
+
+    
+}
